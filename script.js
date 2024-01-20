aa0d0734d4f88dd7b3d49447b92686826ff5f56b
Ryan Macalandag <rmacalandag@gmail.com>
2024-01-20 23:03:49 +1100

    First publish

---
+++ script.js
@@ -0,0 +1,18 @@
+const questions = document.querySelector('.questions');
+
+questions.addEventListener('click', (e) => {
+  if(e.target.classList.contains('btn')) {
+    console.log(e.target)
+
+    e.target.parentNode.classList.toggle('hidden');
+
+    if(e.target.classList.contains('open')) {
+      e.target.src = '/assets/images/icon-plus.svg';
+      e.target.classList.toggle('open');
+    } else {
+      e.target.src = '/assets/images/icon-minus.svg';
+      e.target.classList.toggle('open');
+    }
+
+  } 
+})  
